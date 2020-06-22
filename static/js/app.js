7777f38a92741429036ae2cc8701804e6bdd22ab
Lakshmi Prasanna Rangam <LakshmiPrasannaRangam>
2020-06-21 18:27:59 -0600

    added some code

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,28 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+function updateTable(data){
+    tbody.html("");
+    data.forEach((row) => {
+    var tablerow = tbody.append("tr");
+    Object.values(row).forEach((value)=>{
+    var tablecell= tablerow.append("td");
+    tablecell.text(value);
+    }    
+    );   
+    });
+}
+function filterTable(){
+    var filterData = tableData;
+    var date = d3.select("#datetime").property("value");
+    if (date){
+        filterData = filterData.filter(result => result.datetime === date);
+        updateTable(filterData);
+   } 
+
+}
+d3.select("#filter-btn").on("click",filterTable);
+updateTable(tableData);
+
